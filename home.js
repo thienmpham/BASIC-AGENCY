b56ab368bbddfb6364cacd72012cee853457300b
thienmpham <thienmpham@gmail.com>
2024-07-04 13:13:23 -0400

    Working on slider animation when clicking on dropdown button

--- home.js
+++ home.js
@@ -3,8 +3,24 @@ function exitDrop(buttonSelector, parentSelector) {
     let button = document.querySelector(buttonSelector);
     let parent = document.querySelector(parentSelector);
     button.addEventListener('click', function(){
-        parent.style.visibility = 'hidden';
-        parent.style.pointerEvents = 'none';
+        parent.classList.toggle('visible');
     })
 }
 exitDrop('#exit-button', '#dropdown-page');
+exitDrop('#dropdown', '#dropdown-page');
+
+function dropdownAnimation() {
+    let exitBtn = document.querySelector('#exit-button');
+    let dropdownBtn = document.querySelector('#dropdown');
+    let animation = document.querySelector('#animation-slide');
+
+    document.addEventListener('click', function(){
+        if(dropdownBtn) {
+            animation.classList.add('slider');
+        }
+        if(exitBtn) {
+            animation.classList.remove('slider');
+        }
+    })
+}
+dropdownAnimation();
